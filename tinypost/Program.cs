3efe206b84ca5c640b6a1f3476d4bf8831b5d860
Rob Mensching <rob@robmensching.com>
2013-04-17 08:43:48 -0700

    Add hack of command-line parsing to test plus copyright headers.

--- tinypost/Program.cs
+++ tinypost/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace poster
+﻿// <copyright file="Program.cs" company="RobMensching.com LLC">
+//    Copyright (c) RobMensching.com LLC.  All rights reserved.
+// </copyright>
+
+namespace poster
 {
     using System;
     using System.Collections.Generic;
@@ -8,7 +12,42 @@
     {
         public static void Main(string[] args)
         {
-            IEnumerable<Document> posted = new AtomPoster(new Uri("http://example.com")).Process(".");
+            Poster poster = null;
+            string path = ".";
+
+            for (int i = 0; i < args.Length; ++i)
+            {
+                if (args[i].Equals("atom", StringComparison.OrdinalIgnoreCase))
+                {
+                    Uri uri = new Uri(args[++i]);
+                    string username = null;
+                    string password = null;
+                    if (args[i + 1].Equals("-u", StringComparison.OrdinalIgnoreCase))
+                    {
+                        ++i;
+                        username = args[++i];
+                    }
+
+                    if (args[i + 1].Equals("-p", StringComparison.OrdinalIgnoreCase))
+                    {
+                        ++i;
+                        password = args[++i];
+                    }
+
+                    poster = new AtomPoster(uri, username, password);
+                }
+                else
+                {
+                    path = args[i];
+                }
+            }
+
+            if (poster == null)
+            {
+                return;
+            }
+
+            IEnumerable<Document> posted = poster.Process(path);
             foreach (var doc in posted)
             {
                 Console.WriteLine("Posted: {1}\r\nat: {2}\r\nfrom: {0}\r\n", doc.Path, doc.Title, doc.Published);
